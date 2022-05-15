7b81f526a68b025f08db86bb8d8c2b38fe2835b2
sukanyagovindan <gsukanya@gmail.com>
2022-05-14 20:28:44 -0700

    commit tree df49e7bd7c54aa88e46faa9b5d55930a10259bf1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const stringArray = ["Charlie", "Samip", "Ali"]
+function writeCards(stringArray, eventName) {
+    const returnArray = [];
+    for (let i=0; i<stringArray.length; i++) {
+        returnArray[i] = `Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return returnArray;
+}
+
+function countDown(someint) {
+    while (someint>=0) {
+        console.log(someint--);
+    }
+}
+
